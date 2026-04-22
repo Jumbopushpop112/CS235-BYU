eaf7325512d3e4f51e3e356fb018c36b37dc86e9
Matthew Hart <68395720+Jumbopushpop112@users.noreply.github.com>
2026-04-21 18:50:14 -0600

    Hello

---
+++ FinalExam/final/avl/AVL.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <functional>
+
+template <class T>
+class AVL
+{
+public:
+    AVL()
+    {
+    }
+
+    ~AVL()
+    {
+    }
+
+    bool insert(T item)
+    {
+    }
+
+    bool remove(T item)
+    {
+    }
+
+    bool contains(T item) const
+    {
+    }
+
+    void clear()
+    {
+    }
+
+    int size() const
+    {
+    }
+
+    void inorder_traversal(std::function<void(T)> visit) const
+    {
+    }
+
+    void preorder_traversal(std::function<void(T)> visit) const
+    {
+    }
+
+    void postorder_traversal(std::function<void(T)> visit) const
+    {
+    }
+};
---
+++ FinalExam/final/quicksort/QS.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <vector>
+
+template <class T>
+void sort(std::vector<T> &array)
+{
+}
